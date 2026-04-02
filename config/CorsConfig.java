54796877c9ff56f34141c6d0983e5f3b835a12f9
PALADUGU SAITEJASRI <23eg105p52@anurag.edu.in>
2026-04-02 17:09:45 +0530

    Update CorsConfig.java

--- config/CorsConfig.java
+++ config/CorsConfig.java
@@ -1,1 +1,17 @@
 
+package com.bybills.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://bybills-frontend-4.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
