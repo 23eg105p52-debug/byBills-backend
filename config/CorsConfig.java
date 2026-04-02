0bae918d865d60c209b0a1341f039b9c3e4f80ea
PALADUGU SAITEJASRI <23eg105p52@anurag.edu.in>
2026-04-02 17:12:47 +0530

    Update CorsConfig.java

--- config/CorsConfig.java
+++ config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://bybills-frontend-4.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
