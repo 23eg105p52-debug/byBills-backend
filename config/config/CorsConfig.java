ce7c3644321b22594a69b67a2de06140b2d9e2ca
PALADUGU SAITEJASRI <23eg105p52@anurag.edu.in>
2026-04-02 17:04:18 +0530

    Update CorsConfig.java

--- config/config/CorsConfig.java
+++ config/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.bybillsbackend.config;
+package com.bybills;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
